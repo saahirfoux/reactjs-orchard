96cfd4fc4e1e4c6e358aab1c98573f3fe21d49a7
saahirfoux <saahirhfoux@gmail.com>
2020-07-18 19:05:28 -0400

    TypeScript + Eslint
    Added typescript and eslint to the server. Included basic config options for both.

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 9000;
+
+const one = 1;
+const two = 2;
+
+app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
